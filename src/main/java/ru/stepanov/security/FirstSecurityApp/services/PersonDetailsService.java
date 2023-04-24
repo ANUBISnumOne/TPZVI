acf4834ec8c299190e53327ad691d4fe56c73bbf
ANUBISnumOne <stepanovsa2007@yandex.ru>
2023-04-24 21:07:54 +0300

    init commit

---
+++ src/main/java/ru/stepanov/security/FirstSecurityApp/services/PersonDetailsService.java
@@ -0,0 +1,34 @@
+package ru.stepanov.security.FirstSecurityApp.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import ru.stepanov.security.FirstSecurityApp.models.Person;
+import ru.stepanov.security.FirstSecurityApp.repositories.PeopleRepository;
+import ru.stepanov.security.FirstSecurityApp.security.PersonDetails;
+
+import java.util.Optional;
+
+
+@Service
+public class PersonDetailsService implements UserDetailsService {
+
+    private final PeopleRepository peopleRepository;
+
+    @Autowired
+    public PersonDetailsService(PeopleRepository peopleRepository) {
+        this.peopleRepository = peopleRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        Optional<Person> person = peopleRepository.findByUsername(s);
+
+        if (person.isEmpty())
+            throw new UsernameNotFoundException("User not found");
+
+        return new PersonDetails(person.get());
+    }
+}
---
+++ src/test/java/ru/stepanov/security/FirstSecurityApp/SpringSecurityAppTests.java
@@ -0,0 +1,13 @@
+package ru.stepanov.security.FirstSecurityApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
